1acf93ed4dd3aab83da520263e958fe99ac9130e
rmsyn <rmsynchls@gmail.com>
2023-10-16 03:29:15 +0000

    fixup: i2c: better field definition - `TAR` and `SAR`
    Better field definitions for `TAR` and `SAR` register bitfields.
    
    Enables accessor functions for target and slave addresses that are 7-bit
    and 10-bit specific.

--- jh7110-vf2-13b-pac/src/snps_designware_i2c_0/sar.rs
+++ jh7110-vf2-13b-pac/src/snps_designware_i2c_0/sar.rs
@@ -2,23 +2,38 @@
 pub type R = crate::R<SAR_SPEC>;
 #[doc = "Register `sar` writer"]
 pub type W = crate::W<SAR_SPEC>;
-#[doc = "Field `sar` reader - sar"]
-pub type SAR_R = crate::FieldReader<u32>;
-#[doc = "Field `sar` writer - sar"]
-pub type SAR_W<'a, REG, const O: u8> = crate::FieldWriter<'a, REG, 32, O, u32>;
+#[doc = "Field `address_7bit` reader - Slave address, 7-bit mode"]
+pub type ADDRESS_7BIT_R = crate::FieldReader;
+#[doc = "Field `address_7bit` writer - Slave address, 7-bit mode"]
+pub type ADDRESS_7BIT_W<'a, REG, const O: u8> = crate::FieldWriter<'a, REG, 7, O>;
+#[doc = "Field `address_10bit` reader - Slave address, 10-bit mode"]
+pub type ADDRESS_10BIT_R = crate::FieldReader<u16>;
+#[doc = "Field `address_10bit` writer - Slave address, 10-bit mode"]
+pub type ADDRESS_10BIT_W<'a, REG, const O: u8> = crate::FieldWriter<'a, REG, 10, O, u16>;
 impl R {
-    #[doc = "Bits 0:31 - sar"]
+    #[doc = "Bits 0:6 - Slave address, 7-bit mode"]
     #[inline(always)]
-    pub fn sar(&self) -> SAR_R {
-        SAR_R::new(self.bits)
+    pub fn address_7bit(&self) -> ADDRESS_7BIT_R {
+        ADDRESS_7BIT_R::new((self.bits & 0x7f) as u8)
+    }
+    #[doc = "Bits 0:9 - Slave address, 10-bit mode"]
+    #[inline(always)]
+    pub fn address_10bit(&self) -> ADDRESS_10BIT_R {
+        ADDRESS_10BIT_R::new((self.bits & 0x03ff) as u16)
     }
 }
 impl W {
-    #[doc = "Bits 0:31 - sar"]
+    #[doc = "Bits 0:6 - Slave address, 7-bit mode"]
     #[inline(always)]
     #[must_use]
-    pub fn sar(&mut self) -> SAR_W<SAR_SPEC, 0> {
-        SAR_W::new(self)
+    pub fn address_7bit(&mut self) -> ADDRESS_7BIT_W<SAR_SPEC, 0> {
+        ADDRESS_7BIT_W::new(self)
+    }
+    #[doc = "Bits 0:9 - Slave address, 10-bit mode"]
+    #[inline(always)]
+    #[must_use]
+    pub fn address_10bit(&mut self) -> ADDRESS_10BIT_W<SAR_SPEC, 0> {
+        ADDRESS_10BIT_W::new(self)
     }
     #[doc = "Writes raw bits to the register."]
     #[inline(always)]
--- jh7110-vf2-13b-pac/src/snps_designware_i2c_3/tar.rs
+++ jh7110-vf2-13b-pac/src/snps_designware_i2c_3/tar.rs
@@ -2,23 +2,53 @@
 pub type R = crate::R<TAR_SPEC>;
 #[doc = "Register `tar` writer"]
 pub type W = crate::W<TAR_SPEC>;
-#[doc = "Field `master_10bit` reader - If I2C_DYNAMIC_TAR_UPDATE is set, the 10-bit addressing mode has to be enabled via bit 12 of TAR register. We set it always as I2C_DYNAMIC_TAR_UPDATE can't be detected from registers."]
-pub type MASTER_10BIT_R = crate::BitReader;
-#[doc = "Field `master_10bit` writer - If I2C_DYNAMIC_TAR_UPDATE is set, the 10-bit addressing mode has to be enabled via bit 12 of TAR register. We set it always as I2C_DYNAMIC_TAR_UPDATE can't be detected from registers."]
-pub type MASTER_10BIT_W<'a, REG, const O: u8> = crate::BitWriter<'a, REG, O>;
+#[doc = "Field `address_7bit` reader - Target address, 7-bit mode"]
+pub type ADDRESS_7BIT_R = crate::FieldReader;
+#[doc = "Field `address_7bit` writer - Target address, 7-bit mode"]
+pub type ADDRESS_7BIT_W<'a, REG, const O: u8> = crate::FieldWriter<'a, REG, 7, O>;
+#[doc = "Field `address_10bit` reader - Target address, 10-bit mode"]
+pub type ADDRESS_10BIT_R = crate::FieldReader<u16>;
+#[doc = "Field `address_10bit` writer - Target address, 10-bit mode"]
+pub type ADDRESS_10BIT_W<'a, REG, const O: u8> = crate::FieldWriter<'a, REG, 10, O, u16>;
+#[doc = "Field `mode` reader - Target addressing mode - 0: 7-bit, 1: 10-bit"]
+pub type MODE_R = crate::BitReader;
+#[doc = "Field `mode` writer - Target addressing mode - 0: 7-bit, 1: 10-bit"]
+pub type MODE_W<'a, REG, const O: u8> = crate::BitWriter<'a, REG, O>;
 impl R {
-    #[doc = "Bit 12 - If I2C_DYNAMIC_TAR_UPDATE is set, the 10-bit addressing mode has to be enabled via bit 12 of TAR register. We set it always as I2C_DYNAMIC_TAR_UPDATE can't be detected from registers."]
+    #[doc = "Bits 0:6 - Target address, 7-bit mode"]
     #[inline(always)]
-    pub fn master_10bit(&self) -> MASTER_10BIT_R {
-        MASTER_10BIT_R::new(((self.bits >> 12) & 1) != 0)
+    pub fn address_7bit(&self) -> ADDRESS_7BIT_R {
+        ADDRESS_7BIT_R::new((self.bits & 0x7f) as u8)
+    }
+    #[doc = "Bits 0:9 - Target address, 10-bit mode"]
+    #[inline(always)]
+    pub fn address_10bit(&self) -> ADDRESS_10BIT_R {
+        ADDRESS_10BIT_R::new((self.bits & 0x03ff) as u16)
+    }
+    #[doc = "Bit 12 - Target addressing mode - 0: 7-bit, 1: 10-bit"]
+    #[inline(always)]
+    pub fn mode(&self) -> MODE_R {
+        MODE_R::new(((self.bits >> 12) & 1) != 0)
     }
 }
 impl W {
-    #[doc = "Bit 12 - If I2C_DYNAMIC_TAR_UPDATE is set, the 10-bit addressing mode has to be enabled via bit 12 of TAR register. We set it always as I2C_DYNAMIC_TAR_UPDATE can't be detected from registers."]
+    #[doc = "Bits 0:6 - Target address, 7-bit mode"]
     #[inline(always)]
     #[must_use]
-    pub fn master_10bit(&mut self) -> MASTER_10BIT_W<TAR_SPEC, 12> {
-        MASTER_10BIT_W::new(self)
+    pub fn address_7bit(&mut self) -> ADDRESS_7BIT_W<TAR_SPEC, 0> {
+        ADDRESS_7BIT_W::new(self)
+    }
+    #[doc = "Bits 0:9 - Target address, 10-bit mode"]
+    #[inline(always)]
+    #[must_use]
+    pub fn address_10bit(&mut self) -> ADDRESS_10BIT_W<TAR_SPEC, 0> {
+        ADDRESS_10BIT_W::new(self)
+    }
+    #[doc = "Bit 12 - Target addressing mode - 0: 7-bit, 1: 10-bit"]
+    #[inline(always)]
+    #[must_use]
+    pub fn mode(&mut self) -> MODE_W<TAR_SPEC, 12> {
+        MODE_W::new(self)
     }
     #[doc = "Writes raw bits to the register."]
     #[inline(always)]
